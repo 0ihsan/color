fcd3992a007eb77f0b0380fa87eb3deb6f6769f6
ihsan <ihsanl@pm.me>
2022-02-06 20:56:29 +0300

    add color test

--- color.go
+++ color.go
@@ -1,11 +1,5 @@
 package color
 
-func Ansi(ansicode string) func(string) string {
-	return func(msg string) string {
-		return "\033["+ansicode+"m"+msg+"\033[0m"
-	}
-}
-
 var Red         = Ansi("31")
 var Green       = Ansi("32")
 var Yellow      = Ansi("33")
@@ -21,3 +15,10 @@ var LightBlue   = Ansi("94")
 var LightPurple = Ansi("95")
 var LightAqua   = Ansi("96")
 var White       = Ansi("97")
+
+func Ansi(ansicode string) func(string) string {
+	return func(msg string) string {
+		return "\033["+ansicode+"m"+msg+"\033[0m"
+	}
+}
+
---
+++ color_test.go
@@ -0,0 +1,21 @@
+package color
+
+import "testing"
+
+func Test(t *testing.T) {
+	println(Red("Red"))
+	println(Green("Green"))
+	println(Yellow("Yellow"))
+	println(Blue("Blue"))
+	println(Purple("Purple"))
+	println(Aqua("Aqua"))
+	println(Lightgray("Lightgray"))
+	println(Gray("Gray"))
+	println(LightRed("LightRed"))
+	println(LightGreen("LightGreen"))
+	println(LightYellow("LightYellow"))
+	println(LightBlue("LightBlue"))
+	println(LightPurple("LightPurple"))
+	println(LightAqua("LightAqua"))
+	println(White("White"))
+}
